9195acb7caed85eee3c464d17370c560f7592bdf
Joel Campbell <campbell0816@gmail.com>
2018-04-25 15:40:18 +1000

    new code

--- scripts.py
+++ scripts.py
@@ -56,4 +56,91 @@ print("Investment 1 is worth " + str(round(-investment_1, 2)) + " in today's dol
 investment_2 = np.pv(rate=0.05, nper=10, pmt=0, fv=10000)
 print("Investment 2 is worth " + str(round(-investment_2, 2)) + " in today's dollars")
 
+# Calculate the value of two separate investments projecting FV for two interest rates note the negative (-) value for PV
+
+import numpy as np
+
+# Caculate investment_1
+investment_1 = np.fv(rate=0.05, nper=15, pmt=0, pv=-10000)
+print("Investment 1 will yield a total of $" + str(round(investment_1, 2)) + " in 15 years")
+
+# Caculate investment_2
+investment_2 = np.fv(rate=0.08, nper=15, pmt=0, pv=-10000)
+print("Investment 2 will yield a total of $" + str(round(investment_2, 2)) + " in 15 years")
+
+# Calculate the FV of an investment and then discount by projected inflation rate
+
+import numpy as np
+
+# Calculate investment_1
+investment_1 = np.fv(rate=0.08, nper=10, pmt=0, pv=-10000)
+print("Investment 1 will yield a total of $" + str(round(investment_1, 2)) + " in 10 years")
+
+# Calculate investment_2
+investment_1_discounted = np.pv(rate=0.03, nper=10, pmt=0, fv=investment_1)
+print("After adjusting for inflation, investment 1 is worth $" + str(round(-investment_1_discounted, 2)) + " in today's dollars")
+
+# Discount a series of cash flows to calculate NPV
+import numpy as np
+
+# Predefined array of cash flows
+cash_flows = np.array([100, 100, 100, 100, 100])
+
+# Calculate investment_1
+investment_1 = np.npv(rate=0.03, values=cash_flows)
+print("Investment 1's net present value is $" + str(round(investment_1, 2)) + " in today's dollars")
+
+# Calculate investment_2
+investment_2 = np.npv(rate=0.05, values=cash_flows)
+print("Investment 2's net present value is $" + str(round(investment_2, 2)) + " in today's dollars")
+
+# Calculate investment_3
+investment_3 = np.npv(rate=0.07, values=cash_flows)
+print("Investment 3's net present value is $" + str(round(investment_3, 2)) + " in today's dollars")
+
+# Calculate the NPV value of a series of cash flows from arrays
+import numpy as np
+
+# Create an array of cash flows for project 1
+cash_flows_1 = np.array([-250, 100, 200, 300, 400])
+
+# Create an array of cash flows for project 2
+cash_flows_2 = np.array([-250, 300, -250, 300, 300])
+
+# Calculate the net present value of project 1
+investment_1 = np.npv(rate=0.03, values=cash_flows_1)
+print("The net present value of Investment 1 is worth $" + str(round(investment_1, 2)) + " in today's dollars")
+
+# Calculate the net present value of project 2
+investment_2 = np.npv(rate=0.03, values=cash_flows_2)
+print("The net present value of Investment 2 is worth $" + str(round(investment_2, 2)) + " in today's dollars")
+
+# Calculate the impact of discount at different thresholds of years
+import numpy as np
+
+# Calculate investment_1
+investment_1 = np.pv(rate=0.03, nper=30, pmt=0, fv=100)
+print("Investment 1 is worth $" + str(round(-investment_1, 2)) + " in today's dollars")
+
+# Calculate investment_2
+investment_2 = np.pv(rate=0.03, nper=50, pmt=0, fv=100)
+print("Investment 2 is worth $" + str(round(-investment_2, 2)) + " in today's dollars")
+
+# Calculate investment_3
+investment_3 = np.pv(rate=0.03, nper=100, pmt=0, fv=100)
+print("Investment 3 is worth $" + str(round(-investment_3, 2)) + " in today's dollars")
+
+# Create arrays in multiples of 1000
+import numpy as np
+
+# Create a numpy array of cash flows for Project 1
+cf_project_1 = np.array([-1000, 200, 250, 300, 350, 400, 450, 500, 550, 600])
+
+# Create a numpy array of cash flows for Project 2
+cf_project_2 = np.array([-1000, 150, 225, 300, 375, 425, 500, 575, 600, 625])
+
+# Scale the original objects by 1000x
+cf_project1 = cf_project_1 * 1000
+cf_project2 = cf_project_2 * 1000
+
 
